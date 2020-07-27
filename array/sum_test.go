0fa77ecc6526173a4d517d647c2a057724d0eb93
abolfazlmim <afmmyam@gmail.com>
2020-07-27 22:28:07 +0400

    99-05-06

--- array/sum_test.go
+++ array/sum_test.go
@@ -2,26 +2,40 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
 func ExampleSum() {
-	numbers := [5]int{1, 2, 3, 4, 65}
+	numbers := []int{1, 2, 3, 4, 65}
 
 	sum := Sum(numbers)
 	fmt.Println(sum)
 }
 
-
-
 func TestSum(t *testing.T) {
-	t.Run("collection of 5 number s", func(t *testing.T){
-		numbers :=[]int
+	t.Run("collection of 5 number s", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
+		if got != want {
+			t.Errorf("got %d want %d given , %v", got, want, numbers)
+		}
 	})
-	numbers := [5]int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	want := 15
-	if got != want {
-		t.Errorf("got %d want %d given , %v", got, want, numbers)
+	t.Run("collection of any size ", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5, 6}
+		got := Sum(numbers)
+		want := 21
+		if got != want {
+			t.Errorf("got %d want %d given , %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("got %v  want %v", got, want)
 	}
 }
